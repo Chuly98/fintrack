e334f54b6378884bb612b05dc5e8cf67e50a6075
Chuly98 <markspk98@gmail.com>
2026-09-06 23:35:10 -0300

    Compilacion exitosa para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fintrack/', // <- Esta línea es obligatoria para GitHub Pages
 })
